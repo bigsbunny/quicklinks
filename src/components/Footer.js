fb47be425745be507a931b370a6edfa761a45fbd
Bighnesh Sahoo <bighnesh2001@gmail.com>
2021-11-10 23:54:10 +0530

    Adds Github button for stars

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,18 @@ import React from "react";
 
 function Footer(props) {
     return (
-        <footer 
+        <footer
             className="w-screen p-3 text-center bg-blue-400 fixed bottom-0 text-white text-base"
-            >
-                A project by <a href="https://github.com/bigsbunny"
-                    target="_blank"
-                    className="underline" rel="noreferrer">bigsby</a> | © 2021 Bighnesh Sahoo
+        >
+            A project by <a href="https://github.com/bigsbunny"
+                target="_blank"
+                className="underline" rel="noreferrer">bigsby</a> | © 2021 Bighnesh Sahoo
+
+            <iframe src="https://ghbtns.com/github-btn.html?user=bigsbunny&repo=quicklinks&type=star&count=true" 
+                    frameborder="0" scrolling="0" 
+                    width="100" height="20" 
+                    title="GitHub"
+                    className="inline absolute right-0"></iframe>
         </footer>
     )
 }
